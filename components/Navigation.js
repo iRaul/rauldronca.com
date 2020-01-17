a3f2700160f537e3ff765c2057baa0bcf6d78cf0
iRaul <rauldronk@gmail.com>
2020-01-17 15:11:14 +0200

    navigation component

---
+++ components/Navigation.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  padding: 30px 0;
+  margin-bottom: 100px;
+`;
+
+const NavItem = styled.a`
+  font-size: 1.7rem;
+  font-weight: 200;
+  color: ${props =>
+    props.active ? 'rgba(255, 255, 255, 1)' : 'rgba(255, 255, 255, 0.6)'};
+  transition: color 0.3s ease;
+
+  &:hover {
+    color: rgba(255, 255, 255, 1);
+  }
+`;
+
+const Navigation = () => (
+  <Nav>
+    <NavItem>Home</NavItem>
+    <NavItem>About</NavItem>
+    <NavItem>Projects</NavItem>
+    <NavItem>Contact</NavItem>
+  </Nav>
+);
+
+export default Navigation;
