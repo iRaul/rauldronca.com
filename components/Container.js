503aca10a48171f118f81cc9083a64ebc0e7a244
iRaul <rauldronk@gmail.com>
2020-01-17 15:10:59 +0200

    container component

---
+++ components/Container.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: 768px;
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  padding: 15px;
+  position: relative;
+`;
+
+export default Container;
