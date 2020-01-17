615bb8418c6032fb1d7f9728eb29dca10f4abb1e
iRaul <rauldronk@gmail.com>
2020-01-17 15:12:01 +0200

    started homepage

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
-import Global from '../components/Global';
+import styled from 'styled-components';
+
+import Title from '../components/Title';
+import Layout from '../components/Layout';
+
+const Subtitle = styled.h2`
+  font-size: 2.6rem;
+  font-weight: 300;
+  margin: 0;
+  color: rgba(255, 255, 255, 0.7);
+`;
+
+const CTA = styled.a`
+  display: block;
+  font-size: 1.8rem;
+  font-weight: 300;
+  margin-top: 40px;
+  color: rgba(255, 255, 255, 1);
+`;
 
 export default () => (
   <>
-    <Global />
-
-    <h1>Test</h1>
+    <Layout>
+      <Title>Raul Dronca</Title>
+      <Subtitle>User Interface Designer & Engineer</Subtitle>
+      <CTA>More about me →</CTA>
+    </Layout>
   </>
 );
