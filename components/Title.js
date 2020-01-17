32d9eff249ca1663450311a00be56f7809f342c9
iRaul <rauldronk@gmail.com>
2020-01-17 15:11:20 +0200

    title component

---
+++ components/Title.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: rgba(255, 255, 255, 1);
+  font-size: 5.5rem;
+  font-weight: 600;
+  margin-top: 0;
+  margin-bottom: 6px;
+`;
+
+export default Title;
