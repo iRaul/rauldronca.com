73a83f414c10df486e8c9d551ecd31fad36b5c8f
iRaul <rauldronk@gmail.com>
2020-01-21 15:58:37 +0200

    text paragraph component

---
+++ components/Text.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Text = styled.p`
+  font-size: 1.4rem;
+  font-weight: 200;
+  line-height: 1.6;
+  color: rgba(255, 255, 255, 0.85);
+`;
+
+export default Text;
