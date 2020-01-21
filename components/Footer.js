6f699346528163e70451604977ada6a61739ae8a
iRaul <rauldronk@gmail.com>
2020-01-21 14:46:35 +0200

    footer component

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.footer`
+  color: rgba(255, 255, 255, 0.6);
+  font-size: 1.4rem;
+`;
+
+const Footer = () => <Wrapper>Ⓒ Raul Dronca 2020</Wrapper>;
+
+export default Footer;
