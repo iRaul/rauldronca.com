aee565e653774359aec8d051b1849c0e7798e059
iRaul <rauldronk@gmail.com>
2020-01-22 16:48:52 +0200

    added favicon

--- components/Head.js
+++ components/Head.js
@@ -5,5 +5,17 @@ export default () => (
     <title>Raul Dronca | UI Designer/Engineer</title>
     <meta charSet="utf-8" />
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <link
+      rel="icon"
+      type="image/png"
+      href="../static/favicons/favicon-32x32.png"
+      sizes="32x32"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      href="../static/favicons/favicon-16x16.png"
+      sizes="16x16"
+    />
   </Head>
 );
