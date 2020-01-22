b0502de1645e789a0584c3b10b99f58e12b5071c
iRaul <rauldronk@gmail.com>
2020-01-22 15:32:40 +0200

    extracted subtitle as a component

---
+++ components/Subtitle.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Subtitle = styled.h2`
+  font-size: 2.6rem;
+  font-weight: 300;
+  margin: 0;
+  color: rgba(255, 255, 255, 0.7);
+`;
+
+export default Subtitle;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,8 @@
-import styled from 'styled-components';
-
 import Title from '../components/Title';
+import Subtitle from '../components/Subtitle';
 import Layout from '../components/Layout';
 import Link from '../components/Link';
 import { CTA } from '../components/Buttons';
-
-const Subtitle = styled.h2`
-  font-size: 2.6rem;
-  font-weight: 300;
-  margin: 0;
-  color: rgba(255, 255, 255, 0.7);
-`;
 
 export default () => (
   <>
