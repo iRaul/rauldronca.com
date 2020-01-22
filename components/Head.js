a1cd40dadfa0efe284f0bd4d38abb753ed4d312f
iRaul <rauldronk@gmail.com>
2020-01-22 16:36:12 +0200

    title added

---
+++ components/Head.js
@@ -0,0 +1,9 @@
+import Head from 'next/head';
+
+export default () => (
+  <Head>
+    <title>Raul Dronca | UI Designer/Engineer</title>
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+  </Head>
+);
--- components/Layout.js
+++ components/Layout.js
@@ -3,9 +3,11 @@ import React from 'react';
 import Navigation from './Navigation';
 import Container from './Container';
 import GlobalStyle from './Global';
+import Head from './Head';
 
 const Layout = ({ children }) => (
   <>
+    <Head />
     <GlobalStyle />
     <Container>
       <Navigation />
